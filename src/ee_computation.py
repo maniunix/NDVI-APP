50552c565ce3c77e44b9319e0230e692463e9122
maniunix <manishpadole13@gmail.com>
2023-08-25 12:09:47 +0530

    Migration o fMain dataframe function to get the monthly mean NDVI

--- src/ee_computation.py
+++ src/ee_computation.py
@@ -1,23 +1,45 @@
 import ee
+import geopandas as gpd
+from ee_computation import getNDVI
+import pandas as pd
+
+def maskS2clouds(image):
+    qa = image.select('QA60')
+    cloudBitMask = 1 << 10
+    cirrusBitMask = 1 << 11
+    mask = qa.bitwiseAnd(cloudBitMask).eq(0) \
+        .And(qa.bitwiseAnd(cirrusBitMask).eq(0))
+    return image.updateMask(mask)
 
 
-def getNDVI(image):
+def read_shapefile(file_path: str, start_date, end_date):
     '''
-    Take the image and returns the Normalized difference of the Near-Infrared band and Red Band
-    Input -> ee.Image
+    Takes the File path to shapefile as an input.
+    Returns clipped NDVI of ROI.
+    Input -> Shapefile
     Output -> ee.Image
     '''
-    ndvi = image.normalizedDifference(['B8','B4']).rename('NDVI')
-    return image.addBands(ndvi)
+    # start_date = "2018-01-01"
+    # end_date = "2018-12-31"
+    if "shp" in file_path:
+        # global start_date, end_date
+        gdf = gpd.read_file(file_path)
+        geom = list(gdf.geometry[0].exterior.coords)
+        aoi = ee.Geometry.Polygon(geom)
+        image = ee.ImageCollection('COPERNICUS/S2').filterBounds(aoi).filterDate(start_date, end_date)
+        ndvi_collection = image.map(maskS2clouds).map(getNDVI).filterDate(start_date, end_date)
+        return ndvi_collection
 
-def monthlyNDVI(n,collection ,startDate, aoi):
-    date = ee.Date(startDate).advance(n,'month')
+def monthlyNDVI(n):
+    global ndvi_collection, aoi, start_date
+    date = ee.Date(start_date).advance(n,'month')
     m = date.get("month")
     y = date.get("year")
     dic = ee.Dictionary({
         'Date':date.format('yyyy-MM')
-        })
-    tempNDVI = (collection.filter(ee.Filter.calendarRange(y, y, 'year'))
+    })
+    
+    tempNDVI = (ndvi_collection.select('NDVI').filter(ee.Filter.calendarRange(y, y, 'year'))
                 .filter(ee.Filter.calendarRange(m, m, 'month'))
                 .mean()
                 .reduceRegion(
@@ -26,3 +48,26 @@ def monthlyNDVI(n,collection ,startDate, aoi):
                     scale = 10))
     return dic.combine(tempNDVI)
 
+def getDataframe(file_path, start_date, end_date):
+    gdf = gpd.read_file(file_path)
+    geom = list(gdf.geometry[0].exterior.coords)
+    aoi = ee.Geometry.Polygon(geom)
+    # start_date = "2018-01-01"
+    # end_date = "2022-12-31"
+    ndvi_collection = read_shapefile(file_path, start_date, end_date).map(maskS2clouds)
+    yrMo = ee.List.sequence(0, 12*3-1).map(monthlyNDVI)
+    df = pd.DataFrame(yrMo.getInfo())
+    return df
+
+
+if __name__ == "__main__":
+    file_path = "E:/upwork/1225/1225.shp"
+    ee.Initialize()
+    gdf = gpd.read_file(file_path)
+    geom = list(gdf.geometry[0].exterior.coords)
+    aoi = ee.Geometry.Polygon(geom)
+    start_date = "2018-01-01"
+    end_date = "2022-12-31"
+    ndvi_collection = read_shapefile(file_path, start_date, end_date).map(maskS2clouds)
+    yrMo = ee.List.sequence(0, 12*3-1).map(monthlyNDVI)
+    df = pd.DataFrame(yrMo.getInfo())
